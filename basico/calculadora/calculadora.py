ad48d490016ab1c118f28684f76bf6f11258d958
ManeCH14 <108769119+ManeCH14@users.noreply.github.com>
2026-03-18 09:36:41 -0600

    Implement basic calculator with user options in console

--- basico/calculadora/calculadora.py
+++ basico/calculadora/calculadora.py
@@ -1,1 +1,43 @@
+print('*** Calculadora en python ***')
 
+salir = False
+
+while not salir:
+    try:
+        opcion = int(input('''Operaciones que puedes realizar:
+    1.- Suma
+    2.- Resta
+    3.- Multiplicar
+    4.- Dividir
+    5.- Salir
+Ingresa la opcion:
+    '''))
+    except ValueError:
+        print("Debes ingresar un número válido")
+        continue
+
+    if 1 <= opcion <= 4:
+        num_1 = float(input('Ingrese el primer numero: '))
+        num_2 = float(input('Ingrese el segundo numero: '))
+
+    if opcion == 1:
+        print(f'Resultado: {num_1} + {num_2} = {num_1 + num_2}')
+
+    elif opcion == 2:
+        print(f'Resultado: {num_1} - {num_2} = {num_1 - num_2}')
+
+    elif opcion == 3:
+        print(f'Resultado: {num_1} * {num_2} = {num_1 * num_2}')
+
+    elif opcion == 4:
+        if num_2 == 0:
+            print("Error: no se puede dividir entre cero")
+        else:
+            print(f'Resultado: {num_1} / {num_2} = {num_1 / num_2}')
+
+    elif opcion == 5:
+        salir = True
+        print('Saliendo de la calculadora, Adiós...')
+
+    else:
+        print('Opción inválida')
